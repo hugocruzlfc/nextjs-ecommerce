6284f358b203c95ad2466e142b8d47be2398954e
Hugo Cruz <hugocruzlfc@gmail.com>
2024-07-27 11:48:25 +0200

    feat: add components in main page

--- src/app/add-product/page.tsx
+++ src/app/add-product/page.tsx
@@ -1,30 +1,9 @@
 import FormSubmitButton from "@/components/FormSubmitButton";
-import prisma from "@/lib/prisma";
-
-import { redirect } from "next/navigation";
+import { addProduct } from "@/actions/add-product.action";
 
 export const metadata = {
   title: "Add Product - Flowmazon",
 };
-
-async function addProduct(formData: FormData) {
-  "use server";
-
-  const name = formData.get("name")?.toString();
-  const description = formData.get("description")?.toString();
-  const imageUrl = formData.get("imageUrl")?.toString();
-  const price = Number(formData.get("price") || 0);
-
-  if (!name || !description || !imageUrl || !price) {
-    throw Error("Missing required fields");
-  }
-
-  await prisma.product.create({
-    data: { name, description, imageUrl, price },
-  });
-
-  redirect("/");
-}
 
 export default function AddProductPage() {
   return (
